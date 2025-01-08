27eea64bd6436421178a3cd870532abbdedf3ad0
Maria Jose Salazar <mario10salazar.1995@gmail.com>
2025-01-08 07:29:11 -0500

    fix(database): Add code in the sqlserver.connection.ts file

---
+++ src/database/sqlserver.connection.ts
@@ -0,0 +1,26 @@
+import { Logger } from '@nestjs/common';
+import * as mssql from 'mssql';
+import { envs } from 'src/settings';
+
+const logger = new Logger('SQL Server');
+
+export const connectToSQLServer = async () => {
+  const config: mssql.config = {
+    user: envs.sqlserver_username,
+    password: envs.sqlserver_password,
+    server: envs.sqlserver_hostname,
+    database: envs.sqlserver_database,
+    port: envs.sqlserver_port,
+    options: {
+      encrypt: false,
+      trustServerCertificate: true
+    }
+  };
+  try {
+    await mssql.connect(config);
+    logger.log('Connect to SQL Server successfully!');
+  } catch (error) {
+    logger.error(`Connect to SQL Server failed: ${error.message}`);
+    throw error;
+  }
+}
