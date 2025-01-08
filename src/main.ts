8e8cc079990bfc6ca6eefb62258f762468c823bf
Mario Salazar <mariosalazar.ms.10@gmail.com>
2025-01-08 02:42:57 -0500

    feat(settings): Connect to the database from the main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './settings';
+import { Logger } from '@nestjs/common';
+import * as morgan from 'morgan';
+import { connectToMongoDB } from './database/mongodb.connection';
+import { connectToMySQL } from './database/mysql.connection';
+
+const mg = connectToMongoDB();
+const mysql = connectToMySQL();
+
+console.log(mg);
+console.log(mysql);
 
 async function bootstrap() {
+  const logger = new Logger('Main');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(morgan('dev'));
+  await app.listen(envs.port);
+  logger.log(`App is running on port: ${envs.port}`);
 }
 bootstrap();
