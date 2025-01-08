f86343539c3852c2d92415d9d4b99b6d20c69a77
Mario Salazar <mariosalazar.ms.10@gmail.com>
2025-01-08 07:47:39 -0500

    feat(database): Add comment test branch-06

--- src/database/mongodb.connection.ts
+++ src/database/mongodb.connection.ts
@@ -4,7 +4,7 @@ import { envs } from 'src/settings';
 
 
 const logger = new Logger('MONGODB')
-
+// ! New comment test branch-03 -> branch-06
 export const connectToMongoDB = async () => {
   try {
     await connect(envs.mongo_uri);
